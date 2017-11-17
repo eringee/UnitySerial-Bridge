3a948933661ed1608361a450eab83b7913b3dcfe
eringee <erin.marie.gee@gmail.com>
2017-11-16 20:49:39 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# A UnitySerial Bridge for biodataDuo
+
+This C# script will allow your biodataDuo hardware to communicate with your Unity environment through the serial port.  You can change the baud rate and port name either in the script itself or in Unity's inspector. 
+
+
+# BASIC SETUP
+
+This code was originally created for use with Erin Gee's [biodataDuo](https://github.com/eringee/biodataDuo) hardware system for measuring physiological markers of emotion.
+
+This being said you could probably use these two libraries to bring any kind of physical sensor inputs into your Unity project. 
+
+# WINDOWS SYSTEM 
+
+This code was made in order to avoid the Unity bug wherein the serial chokes in Windows OS
+
+
+# UNITY
+
+This code was tested originally on Unity 2017.2.0f3
+
+# AUTHOR
+
+* [Sofian Audry](http://sofianaudry.com/en)
+
+# COPYING
+Released under GNU GPL 3.0 License.  Copyright Sofian Audry and Erin Gee 2017.
---
+++ biodata_inputscript.cs
@@ -0,0 +1,110 @@
+﻿/******************************************************
+copyright Sofian Audry and Erin Gee 2017
+
+Author Sofian Audry
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For more details: <http://www.gnu.org/licenses/>.
+
+******************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+public class BioData_inputscript : MonoBehaviour {
+
+    public string port = "COM1";   //Change this to suit the name of your port.  Can be done in the editor.
+    public int baudRate = 115200;  //Change this to suit your baudrate.  Can be done in the editor
+
+    SerialPort stream;
+
+    Thread myThread;
+
+	//You can modify and add to these as you like. They are just the variables, in order, that are to be read 
+	//from the serialPort of your microcontroller. 
+
+    public float heart = 0;
+	public float bpm   = 0;
+	public float bvpa  = 0;
+	public float bpma  = 0;
+	public float gsr   = 0;
+
+	// Use this for initialization
+	void Start () {
+        stream = new SerialPort(port, baudRate);
+
+        try
+        {
+            stream.Open();
+
+            Debug.Log("Opened stream: " + stream);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Could not open serial port: " + e.Message);
+
+        }
+
+        // Start thread.
+        myThread = new Thread(new ThreadStart(GetArduino));
+        myThread.Start();
+    }
+
+    void Cleanup()
+    {
+        if (stream != null)
+        {
+            if (stream.IsOpen)
+                stream.Close();
+        }
+
+        stream = null;
+    }
+
+    private void OnApplicationQuit()
+    {
+        Cleanup();
+    }
+
+    public void OnDestroy()
+    {
+        Cleanup();
+    }
+
+    private void GetArduino()
+    {
+        while (myThread.IsAlive)
+        {
+            string line = stream.ReadLine();
+
+            try
+            {
+                string[] values = line.Split('\t');
+                int arg = 0;
+                heart = float.Parse(values[arg++]);
+                bpm = float.Parse(values[arg++]);
+                bvpa = float.Parse(values[arg++]);
+                bpma = float.Parse(values[arg++]);
+                gsr = float.Parse(values[arg++]);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+    }
+
+}
+
