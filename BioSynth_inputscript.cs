34b87b525da5db19cf761998f45e78350c62b40f
Erin Gee <erin.marie.gee@gmail.com>
2019-03-28 12:09:54 +0100

    cleaned up deprecations
    The newer Unity editor informed me that some of the code had deprecated so I fixed that

--- BioSynth_inputscript.cs
+++ BioSynth_inputscript.cs
@@ -1,121 +1,130 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO.Ports;
 using System.Threading;
 
-public class BioSynth_inputscript : MonoBehaviour {
+public class BioSynth_inputscript : MonoBehaviour
+{
 
-	private string portName;     //variable for name of serial port
-	public int baudRate = 9600;  //Change this to suit your baudrate.  Can be done in the editor
+    private string portName;     //variable for name of serial port
+    public int baudRate = 9600;  //Change this to suit your baudrate.  Can be done in the editor
 
-	SerialPort serial;
+    SerialPort serial;
 
-	Thread myThread;
+    Thread myThread;
 
-	//You can modify/add to these as you like, they are just the variables, in order, that are coming from the serialPort
-	//of your microcontroller, separated by Tabs. 
+    //You can modify/add to these as you like, they are just the variables, in order, that are coming from the serialPort
+    //of your microcontroller, separated by Tabs. 
 
-	public float heart = 0; 
-	public float bpm   = 0;  
-	public float bvpa  = 0;  
-	public float bpma  = 0; 
-	public float gsr   = 0;  
+    public float heart = 0;
+    public float bpm = 0;
+    public float bvpa = 0;
+    public float bpma = 0;
+    public float gsr = 0;
 
-	// Initialization
-	void Start () {
+    // Initialization
+    void Start()
+    {
 
-		string portName = GetPortName(); //Figure out which serial port the microcontroller is attached to
+        string portName = GetPortName(); //Figure out which serial port the microcontroller is attached to
 
-		if (portName == "") {
-			print ("Error: Couldn't find serial port.");
-			return;
-		} else {
-	       Debug.Log("Using Serial:" + portName);
-		}
+        if (portName == "")
+        {
+            print("Error: Couldn't find serial port.");
+            return;
+        }
+        else
+        {
+            Debug.Log("Using Serial:" + portName);
+        }
 
-		serial = new SerialPort(portName, baudRate);  //initialize serial port
-		serial.Open();
-		serial.ReadTimeout = 10;
+        serial = new SerialPort(portName, baudRate);  //initialize serial port
+        serial.Open();
+        serial.ReadTimeout = 100;
 
-		// Start thread.
-		myThread = new Thread(new ThreadStart(GetArduino));
-		myThread.Start();
-	} 
+        // Start thread.
+        myThread = new Thread(new ThreadStart(GetArduino));
+        myThread.Start();
+    }
 
-	// Read data from the Arduino
-	private void GetArduino(){
-		while (myThread.IsAlive)
-		{
-			string line = serial.ReadLine();
+    // Read data from the Arduino
+    private void GetArduino()
+    {
+        while (myThread.IsAlive)
+        {
+            string line = serial.ReadLine();
 
-			try
-			{
-				string[] values = line.Split('\t');  //using tab separated values.  See comments for Arduino code.
-				int arg = 0;                         
-				heart = float.Parse(values[arg++]);  //Serial.print(heartSignal); Serial.print("\t")
-				bpm = float.Parse(values[arg++]);    //Serial.print(bpmSignal); Serial.print("\t")
-				bvpa = float.Parse(values[arg++]);   //Serial.print(bvpaSignal); Serial.print("\t")
-				bpma = float.Parse(values[arg++]);   //Serial.print(bpmaSignal); Serial.print("\t")
-				gsr = float.Parse(values[arg++]);    //Serial.println(gsrSignal); 
-			}
-			catch (Exception e)      // this is needed because the Arduino throws weird artifacts sometimes
-			{
-				Debug.Log(e.Message);
-			}
-		}
-	}
+            try
+            {
+                string[] values = line.Split('\t');  //using tab separated values.  See comments for Arduino code.
+                int arg = 0;
+                heart = float.Parse(values[arg++]);  //Serial.print(heartSignal); Serial.print("\t")
+                bpm = float.Parse(values[arg++]);    //Serial.print(bpmSignal); Serial.print("\t")
+                bvpa = float.Parse(values[arg++]);   //Serial.print(bvpaSignal); Serial.print("\t")
+                bpma = float.Parse(values[arg++]);   //Serial.print(bpmaSignal); Serial.print("\t")
+                gsr = float.Parse(values[arg++]);    //Serial.println(gsrSignal); 
+                Debug.Log(heart);
+            }
+            catch (Exception e)      // this is needed because the Arduino throws weird artifacts sometimes
+            {
+                Debug.Log(e.Message);
+            }
+        }
+    }
 
-	string GetPortName() {       
+    string GetPortName()
+    {
 
-		string[] portNames;
+        string[] portNames;
 
-		switch (Application.platform) {
+        switch (Application.platform)
+        {
 
-		case RuntimePlatform.OSXPlayer:
-		case RuntimePlatform.OSXEditor:
-		case RuntimePlatform.LinuxPlayer:
+            case RuntimePlatform.OSXPlayer:
+            case RuntimePlatform.OSXEditor:
+            case RuntimePlatform.LinuxPlayer:
 
-			portNames = System.IO.Ports.SerialPort.GetPortNames();
+                portNames = SerialPort.GetPortNames();
 
-			if (portNames.Length ==0) {
-				portNames = System.IO.Directory.GetFiles("/dev/");                
-			}
+                if (portNames.Length == 0)
+                {
+                    portNames = System.IO.Directory.GetFiles("/dev/");
+                }
 
-			foreach (string portName in portNames) {                                
-				if (portName.StartsWith("/dev/tty.usb") || portName.StartsWith("/dev/ttyUSB")) return portName;
-			}                
-			return ""; 
+                foreach (string portName in portNames)
+                {
+                    if (portName.StartsWith("/dev/tty.usb") || portName.StartsWith("/dev/ttyUSB")) return portName;
+                }
+                return "";
 
-		default: // Windows
+            default: // Windows
 
-			portNames = System.IO.Ports.SerialPort.GetPortNames();
+                portNames = SerialPort.GetPortNames();
 
-			if (portNames.Length > 0) return portNames[0];
-			else return "COM3";
-		}
-	}
+                if (portNames.Length > 0) return portNames[0];
+                else return "COM3";
+        }
+    }
 
-	// close the serial ports if quitting program
-	private void OnApplicationQuit()
-	{
-		Cleanup();
-	}
+    // close the serial ports if quitting program
+    private void OnApplicationQuit()
+    {
+        Cleanup();
+    }
 
-	public void OnDestroy()
-	{
-		Cleanup();
-	}
+    public void OnDestroy()
+    {
+        Cleanup();
+    }
 
-	void Cleanup()  // close the serial ports if there is no Arduino activity
-	{
-		if (serial != null)
-		{
-			if (serial.IsOpen)
-				serial.Close();
-		}
-		serial = null;
-	}
+    void Cleanup()  // close the serial ports if there is no Arduino activity
+    {
+        if (serial != null)
+        {
+            if (serial.IsOpen)
+                serial.Close();
+        }
+        serial = null;
+    }
 
 }
